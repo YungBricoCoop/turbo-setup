2e286b9e0d027abd0688779240c5d8d18fee225c
Elwan <elwan.mayencourt@gmail.com>
2024-04-14 20:23:18 +0200

    ✨ Add user, assign to group

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 import os
+import pwd
+import grp
 import sys
 import typer
 from random import randint
 from typing_extensions import Annotated
 from rich import print
+import subprocess
+import getpass
 
-
+GROUP = "docker"
 FAIL2BAN_CONFIG_FILE = "./fail2ban.conf"
 COWRIE_CONFIG_FILE = "./cowrie.conf"
 COWRIE_DB_FILE = "./cowrie.db"
@@ -48,9 +52,55 @@ def check_os():
 
     print_error("REQUIREMENTS", "This script only works on [b]Linux[/]")
 
+# -------------------- [UTILS] --------------------
+
 def get_random_ssh_port():
     return randint(1024, 10000)
 
+def create_group(group: str):
+    try:
+        grp.getgrnam(group)
+        print_warning("GROUP", f"{group} already exists, proceeding...")
+    except KeyError:
+        os.system(f"sudo groupadd {group}")
+        print_info("GROUP", f"{group} created.")
+
+def create_user(user: str, group: str):
+    try:
+        pwd.getpwnam(user)
+        print_warning("USER", f"{user} already exists, proceeding...")
+    except KeyError:
+        os.system(f"sudo useradd -m {user} -s /bin/bash")
+        os.system(f"sudo usermod -aG {group} {user}")
+        
+        print_info("USER", f"{user} created.")
+
+def create_folder(folder: str):
+    try:
+        os.makedirs(folder)
+        print_info("FOLDER", f"{folder} created.")
+    except FileExistsError:
+        print_warning("FOLDER", f"{folder} already exists, proceeding...")
+
+def chown_folder(folder: str, user: str):
+    os.system(f"sudo chown {user}:{user} {folder}")
+    print_info("FOLDER", f"{folder} permissions set to {user}")
+
+def create_symlink(source: str, destination: str):
+    try:
+        os.symlink(source, destination)
+        print_info("SYMLINK", f"{source} symlinked to {destination}")
+    except FileExistsError:
+        print_warning("SYMLINK", f"{destination} already exists, proceeding...")
+
+def create_deployment_folder(folder: str, user: str):
+    destination = f"/opt/{folder}"
+    home_destination = f"/home/{user}/{folder}"
+    create_folder(destination)
+    create_symlink(destination, home_destination)
+    chown_folder(destination, user)
+    chown_folder(home_destination, user)
+    
 
 def main(
     user: Annotated[str, typer.Argument(help="User used to deploy the app")],
@@ -79,6 +129,11 @@ def main(
     
     check_os()
     check_root()
+
+    create_group(GROUP)
+    create_user(user, GROUP)
+
+    create_deployment_folder(folder, user)
 
 
 if __name__ == "__main__":
